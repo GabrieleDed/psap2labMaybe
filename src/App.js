d1d2901dc565c4a51b61b4090b863fb08501fa69
DESKTOP-LLH4U6F\gaded <g.dedinaite@gmail.com>
2020-10-12 18:10:43 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -7,25 +7,33 @@ class App extends Component {
         characters: [
         
         ],
-      }
+      }     
+    removeCharacter = (index) => {
+        const {characters} = this.state
+    
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        });
+    }
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+}
       render() {
         const { characters } = this.state
       
         return (
-          <div className="container">
-            <Table characterData={characters} removeCharacter={this.removeCharacter} />
-          </div>
-        )
+            <div className="container">
+              <Table characterData={characters} removeCharacter={this.removeCharacter} />
+              <Form />
+              
+              <h3>Add New</h3>
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+          )
       }
-      removeCharacter = (index) => {
-    const {characters} = this.state
-  
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index
-      }),
-    })
-  }
+
 }
   
 export default App
--- src/Form.js
+++ src/Form.js
@@ -1,45 +1,62 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class Form extends Component {
-  initialState = {
-    fname: '',
-    lname: '',
-    possition: '',
-    department: '',
-  }
+    initialState = {
+        fname: '',
+        lname: '',
+        position: '',
+        department: '',
+    }
 
-  state = this.initialState
+    state = this.initialState
+
+    handleChange = (event) => {
+        const { name, value } = event.target
+
+        this.setState({
+            [name]: value,
+        })
+    }
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+      }
+    render() {
+        const { fname, lname, position, department } = this.state;
+
+        return (
+            <form>
+                <label htmlFor="name"> First Name</label>
+                <input
+                    type="text"
+                    name="fname"
+                    id="fname"
+                    value={fname}
+                    onChange={this.handleChange} />
+                <label htmlFor="name"> Last Name</label>
+                    <input
+                        type="text"
+                        name="lname"
+                        id="lname"
+                        value={lname}
+                        onChange={this.handleChange} />
+                <label htmlFor="name"> Possition</label>
+                    <input
+                        type="text"
+                        name="position"
+                        id="position"
+                        value={position}
+                        onChange={this.handleChange} />
+                <label htmlFor="job">Job</label>
+                    <input
+                        type="text"
+                        name="department"
+                        id="department"
+                        value={department}
+                        onChange={this.handleChange} />
+                <input type="button" value="Submit" onClick={this.submitForm} />
+            </form>
+        );
+    }
 }
-  handleChange = (event) => {
-    const {name, value} = event.target
-  
-    this.setState({
-      [name]: value,
-    })
-  }
-  render() {
-    const { name, job } = this.state;
-  
-    return (
-      <form>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={name}
-          onChange={this.handleChange} />
-        <label htmlFor="job">Job</label>
-        <input
-          type="text"
-          name="job"
-          id="job"
-          value={job}
-          onChange={this.handleChange} />
-      </form>
-    );
-  }
-  
-  export default Form;
-
-
+export default Form;
